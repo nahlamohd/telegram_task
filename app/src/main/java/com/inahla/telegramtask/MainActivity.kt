1a5eb4fde0a01da9b80c4a5d6f7b72ea79f25c59
Nahla <nahla@email.com>
2023-03-21 13:06:26 +0200

    Initial commit

---
+++ app/src/main/java/com/inahla/telegramtask/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.inahla.telegramtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
